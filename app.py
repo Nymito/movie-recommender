5d1dc3a36fe4f351706b7edd10542328241b8106
Gianni Jouve <gianni.jouve@gmail.com>
2025-11-07 15:08:30 +0100

    port on 1000 for render

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def recommend():
 if __name__ == "__main__":
     load_resources()
     # debug=False for production; change host/port as needed
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=10000, debug=False)
